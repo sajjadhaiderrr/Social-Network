5b24039525779999338fc2a1395c4340c89f73e4
Hussain Khan <hnkhan@ualberta.ca>
2019-03-24 23:57:38 -0400

    Refactor viewing a single post

--- Conet/posting/post_api.py
+++ Conet/posting/post_api.py
@@ -5,6 +5,7 @@ from posting.models import Post, Comment
 from api.serializers import PostSerializer, CommentSerializer
 from django.shortcuts import render
 from Accounts.models import Author
+from api.ApiHelper import get_friends
 import json
 
 ### API START
@@ -21,6 +22,84 @@ class ReadAllPublicPosts(APIView):
 class ReadSinglePost(APIView):
     # get: Access to a single post with id = `post_id`
     def get(self, request, post_id):
+        #first we check to see if the post with the id exists
+        post = Post.object.get(pk = post_id).exists()
+        if (not post):
+            return Response("Post does not exist.", status=200)
+        
+        #then we check if its unlisted
+        if (post.unlisted == True):
+            return Response("Post is unlisted", status=200)
+        
+        #if the posts visibility is set
+        #to PUBLIC, we are ok to return it
+        if (post.visibility == "PUBLIC"):
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=200)
+
+        #otherwise, the other privacy settings
+        #require that an author be logged in
+
+        #lets check if an author is logged in first
+        
+        if (not Author.objects.get(id=request.user.id).exists()):
+            return Response("Please log in.", status=200)
+        
+        author = request.user.id
+        
+        #if the visibility is set to FRIENDS,
+        #lets check if the current author is in
+        #the posting authors friends list
+        author_of_post = post.author
+        if (post.visibility == "FRIENDS"):
+            friends = get_friends(author_of_post)
+            if (author not in friends):
+                return Response("You are not a friend of the author.", status=200)
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=200)
+        
+        #if the visibility is set to FOAF,
+        #lets check if the current author is in
+        #the posting authors friends of friends list
+        if (post.visibility == "FOAF"):
+            friends = get_friends(author_of_post)
+            friends_of_friends = []
+            for friend in friends:
+                friends_of_friends += get_friends(friend)
+            if (author not in friends_of_friends):
+                return Response("You are not a FOAF of the author.", status=200)
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=200)
+
+        #if the visibility to SERVERONLY,
+        #we check if the current author is in
+        #the posting authors friends list
+        #and we check the posting authors
+        #host and compare it with the current authors
+        if (post.visibility == "SERVERONLY"):
+            local_server = author_of_post.host
+            friends = get_friends(author_of_post)
+            if (author not in friends):
+                return Response("You are not a friend of the author.", status=200)
+            if (author.host != local_server):
+                return Response("You are not on the same server as the author.", status=200)
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=200)
+        
+        #if the visibility is set to PRIVATE,
+        #we check if the current author is in
+        #posts visibileTo field
+        if (post.visibility == "PRIVATE"):
+            if (author not in author_of_post.visibileTo):
+                return Response("You are not allowed to see this post.", status=200)
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=200)
+            
+            
+
+
+    
+
         if (not Post.objects.filter(pk=post_id).exists()):# pylint: disable=maybe-no-member
             return Response(status=200)
         post = Post.objects.filter(pk=post_id)# pylint: disable=maybe-no-member
