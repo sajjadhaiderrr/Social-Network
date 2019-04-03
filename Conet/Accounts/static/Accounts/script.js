033cfccfb92d2c0916daac37c298cd3435f57772
JJack27 <yizhouzhao27@gmail.com>
2019-04-03 13:08:07 -0600

    a

--- Conet/Accounts/static/Accounts/script.js
+++ Conet/Accounts/static/Accounts/script.js
@@ -77,10 +77,11 @@ function addComment(post_url, id, same_host){
     if (same_host){
         header['x-csrftoken'] = csrf_token;
     }else{
-        console.log(post_url);
-        console.log(r.host);
-        console.log(post_url.includes(r.host));
+        
         for (r of remote){
+            console.log(post_url);
+            console.log(r.host);
+            console.log(post_url.includes(r.host));
             if (post_url.includes(r.host)){
                 header["Authorization"] = "Basic " + btoa(r.username + ":" + r.password);
             }
