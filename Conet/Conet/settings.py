1eea8a82079b0a81af156e33697c9d45d95b74fd
JJack27 <yizhouzhao27@gmail.com>
2019-04-03 12:10:41 -0600

    a

--- Conet/Conet/settings.py
+++ Conet/Conet/settings.py
@@ -14,6 +14,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+from corsheaders.defaults import default_headers
+
+CORS_ALLOW_HEADERS = default_headers + (
+    'x-request-user-id',
+)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
