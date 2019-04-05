baa3846fb94b41444883ba328b3e6fff563133f9
AndoryuGuo <guo10@ualberta.ca>
2019-04-04 18:21:37 -0600

    test

--- Conet/api/urls.py
+++ Conet/api/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('author/<uuid:pk>/madeposts', views.AuthorMadePostAPI.as_view(), name='authormadeposts'),
     path('author/<uuid:pk>/posts', views.ViewAuthorPostAPI.as_view(), name='authoridposts'),
 
-    path('author/<uuid:pk>/friends/', views.AuthorFriends.as_view()),
-    path('author/posts/', views.AuthorPostsAPI.as_view())
+    path('service/author/<uuid:pk>/friends/', views.AuthorFriends.as_view()),
+    path('service/author/posts/', views.AuthorPostsAPI.as_view())
 ]
